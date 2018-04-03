fbe3677656eb164a9d0fd889bafd1c5dd3d579ee
Arthur B. de Oliveira <arthurboliveira93@gmail.com>
2018-04-03 15:06:13 -0300

    Working on new Theme; Smal fixes;

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,13 +2,24 @@
 
 public class Block : Destructible
 {
+    private TextMesh txtLife;
+
     public Sprite[] sprites;
+
+    private void Start()
+    {
+        txtLife = GetComponentInChildren<TextMesh>();
+
+        txtLife.text = life.ToString();
+    }
 
     public override void CauseDamage(int dmg)
     {
         base.CauseDamage(dmg);
-        
-        if(life > 0)
-            rend.sprite = sprites[life - 1];
+
+        txtLife.text = life == 0 ? "" : life.ToString();
+
+        //if (life > 0)
+        //    rend.sprite = sprites[life - 1];
     }
 }
