ac9e44bef29b8dcff728504d44e6749fefc97c9e
Arthur B. de Oliveira <arthurboliveira93@gmail.com>
2018-04-04 00:17:00 -0300

    New Scenes; New Theme;

---
+++ Assets/Scripts/Scenes/Loading.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(AsynchronousLoad("Level 1"));
+    }
+
+    IEnumerator AsynchronousLoad(string scene)
+    {
+        yield return null;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            ao.allowSceneActivation = true;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Loading");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void About()
+    {
+        SceneManager.LoadScene("About");
+    }
+
+    public void Infinity()
+    {
+        SceneManager.LoadScene("Infinity");
+    }
+}
